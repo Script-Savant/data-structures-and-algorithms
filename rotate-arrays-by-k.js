92f57dadb5d4c24f7f5f3a9d6d8608e07e30ac55
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-25 20:00:34 +0300

    first solution of rotating

---
+++ rotate-arrays-by-k.js
@@ -0,0 +1,23 @@
+// quest 2 - rotate array by K
+// given an integer array nums, rotate the array to the right by k steps, where k is a non negative number
+
+// input: nums = [1, 2, 3, 4, 5, 6, 7], k = 3 >>> output: [5, 6, 7, 1, 2, 3, 4]
+// input: nums = [-1, -100, 3, 99], k = 2 >>> output: [3, 99, -1, -100]
+
+// [-1, -100, 3, 99] => [99, -1, -100, 3] => [3, 99, -1, -100] ---> this is how the rotation occurs
+
+//solution 1
+function rotateArray(arr, k) {
+  let size = arr.length;
+
+  if (size > k) {
+    k = k % size;
+  }
+
+  const rotated = arr.splice(size - k, size);
+  arr.unshift(...rotated);
+
+  return arr;
+}
+
+console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 3));
